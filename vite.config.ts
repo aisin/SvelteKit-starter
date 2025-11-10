e23075e3bf11630f19f99dd15d7841ecdf69ae49
aisin <prccn@qq.com>
2025-11-11 00:42:01 +0800

    chore(pkg upgrade): 升级依赖与配置（Vite7、Vitest4、Skeleton4 等）

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	test: {
-		workspace: [
+		projects: [
 			{
-				extends: './vite.config.ts',
-				plugins: [svelteTesting()],
+				plugins: [tailwindcss(), sveltekit(), svelteTesting()],
 				test: {
 					name: 'client',
 					environment: 'jsdom',
@@ -20,7 +19,6 @@ export default defineConfig({
 				}
 			},
 			{
-				extends: './vite.config.ts',
 				test: {
 					name: 'server',
 					environment: 'node',
