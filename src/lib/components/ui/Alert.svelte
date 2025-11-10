a59458433c3e642be61ed6009a81d03b09ce6229
aisin <prccn@qq.com>
2025-11-11 03:03:41 +0800

    feat: 增加 shadcn-svelte，并增加 Button&Alert 组件

---
+++ src/lib/components/ui/Alert.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { cn } from '$lib/utils/cn';
+	let { class: className = '', children } = $props();
+</script>
+
+<div
+	role="alert"
+	class={cn(
+		'relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4',
+		className
+	)}
+>
+	{@render children?.()}
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import '../theme.css';
 	import '../app.css';
 	import LightSwitch from './components/LightSwitch.svelte';
 
