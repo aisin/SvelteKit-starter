758a97908fa5449d79f0e27d5cfe5f0fcca86552
aisin <prccn@qq.com>
2025-11-21 00:12:35 +0800

    style: 按钮样式优化

--- src/lib/components/ui/Button.svelte
+++ src/lib/components/ui/Button.svelte
@@ -12,7 +12,7 @@
 					outline:
 						'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
 					secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
-					ghost: 'hover:bg-accent hover:text-accent-foreground',
+					ghost: 'border border-border bg-transparent hover:bg-accent hover:text-accent-foreground',
 					link: 'text-primary underline-offset-4 hover:underline'
 				},
 				size: {
